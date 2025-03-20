414ea25b1ba597766cfe5c0e57e8cdd11046e819
Maxime Moreillon <m.moreillon@gmail.com>
2025-03-20 14:30:40 +0900

    user.properties?.isAdmin

--- controllers/images.ts
+++ controllers/images.ts
@@ -1,33 +1,33 @@
-import Image, { ImageRecord } from "../models/image"
-import createHttpError from "http-errors"
-import { parse_form, enforce_restrictions, getUserId } from "../utils"
-import { imageVariants } from "./imageVariants"
-import { Request, Response } from "express"
+import Image, { ImageRecord } from "../models/image";
+import createHttpError from "http-errors";
+import { parse_form, enforce_restrictions, getUserId } from "../utils";
+import { imageVariants } from "./imageVariants";
+import { Request, Response } from "express";
 import {
   deleteLocalImage,
   saveImageLocally,
   sendLocalImage,
-} from "../storage/local"
+} from "../storage/local";
 import {
   deleteFileFromS3,
   s3Client,
   storeImageToS3,
   streamFileFromS3,
-} from "../storage/s3"
-import { DEFAULT_SERVED_VARIANT } from "./imageVariants"
+} from "../storage/s3";
+import { DEFAULT_SERVED_VARIANT } from "./imageVariants";
 import {
   getImageRecordFromCache,
   removeImageRecordFromCache,
   setImageRecordInCache,
-} from "../cache"
+} from "../cache";
 
 export const upload_image = async (req: Request, res: Response) => {
-  const uploader_id = getUserId(res)
+  const uploader_id = getUserId(res);
 
   const {
     fields: { description, restricted },
     image: { filepath: uploadTempPath, originalFilename: filename, size },
-  }: any = await parse_form(req)
+  }: any = await parse_form(req);
 
   const imageProperties = {
     filename,
@@ -36,17 +36,15 @@ export const upload_image = async (req: Request, res: Response) => {
     uploader_id,
     description,
     restricted,
-  }
+  };
 
-  console.log(description)
+  const record = await Image.create(imageProperties);
 
-  const record = await Image.create(imageProperties)
+  if (s3Client) await storeImageToS3(uploadTempPath, record);
+  else await saveImageLocally(uploadTempPath, record);
 
-  if (s3Client) await storeImageToS3(uploadTempPath, record)
-  else await saveImageLocally(uploadTempPath, record)
-
-  res.send(record)
-}
+  res.send(record);
+};
 
 export const get_image_list = async (req: Request, res: Response) => {
   const {
@@ -55,155 +53,155 @@ export const get_image_list = async (req: Request, res: Response) => {
     order = "-1",
     sort = "upload_date",
     search,
-  }: any = req.query
+  }: any = req.query;
 
-  const query: any = {}
+  const query: any = {};
 
   if (search && search !== "") {
-    const regex = { $regex: search, $options: "i" }
-    const searchableProperties = ["filename", "description"]
-    query.$or = searchableProperties.map((p) => ({ [p]: regex }))
+    const regex = { $regex: search, $options: "i" };
+    const searchableProperties = ["filename", "description"];
+    query.$or = searchableProperties.map((p) => ({ [p]: regex }));
   }
 
   const items = await Image.find(query)
     .skip(Number(skip))
     .sort({ [sort]: order, filename: 1 })
-    .limit(Math.max(Number(limit), 0))
+    .limit(Math.max(Number(limit), 0));
 
-  const total = await Image.countDocuments(query)
+  const total = await Image.countDocuments(query);
 
-  const response = { total, items }
+  const response = { total, items };
 
-  res.send(response)
-}
+  res.send(response);
+};
 
 export const get_image_details = async (req: Request, res: Response) => {
-  const image_id = req.params.id
+  const image_id = req.params.id;
 
   // TODO: auth
 
-  const image = await Image.findById(image_id)
-  if (!image) throw createHttpError(404, `Image not found in DB`)
+  const image = await Image.findById(image_id);
+  if (!image) throw createHttpError(404, `Image not found in DB`);
 
-  res.send(image)
-}
+  res.send(image);
+};
 
 export const get_image = async (req: Request, res: Response) => {
-  const image_id = req.params.id || (req.query.id as string)
+  const image_id = req.params.id || (req.query.id as string);
   const variant =
-    req.params.variant || req.query.variant || DEFAULT_SERVED_VARIANT
+    req.params.variant || req.query.variant || DEFAULT_SERVED_VARIANT;
 
-  let image = await getImageRecordFromCache(image_id)
+  let image = await getImageRecordFromCache(image_id);
 
   if (!image) {
-    image = await Image.findById(image_id)
-    if (!image) throw createHttpError(404, `Image not found in DB`)
-    setImageRecordInCache(image)
+    image = await Image.findById(image_id);
+    if (!image) throw createHttpError(404, `Image not found in DB`);
+    setImageRecordInCache(image);
   }
 
-  enforce_restrictions(image, res)
+  enforce_restrictions(image, res);
 
-  const foundVariant = imageVariants.find(({ name }) => name === variant)
+  const foundVariant = imageVariants.find(({ name }) => name === variant);
   // If no found variant, serve the original image
 
-  if (s3Client) await streamFileFromS3(res, image, foundVariant)
-  else await sendLocalImage(res, image, foundVariant)
+  if (s3Client) await streamFileFromS3(res, image, foundVariant);
+  else await sendLocalImage(res, image, foundVariant);
 
-  save_views(req, image_id)
-}
+  save_views(req, image_id);
+};
 
 export const update_image_details = async (req: Request, res: Response) => {
-  const { user } = res.locals
-  const image_id = req.params.id
-  if (!user) throw createHttpError(403, `Unauthorized to delete image`)
+  const { user } = res.locals;
+  const image_id = req.params.id;
+  if (!user) throw createHttpError(403, `Unauthorized to delete image`);
 
-  const user_id = getUserId(res)
+  const user_id = getUserId(res);
 
-  const user_is_admin = user.isAdmin || user.properties.isAdmin
+  const user_is_admin = user.isAdmin || user.properties?.isAdmin;
 
-  const new_properties = req.body
+  const new_properties = req.body;
 
-  const image = await Image.findById(image_id)
+  const image = await Image.findById(image_id);
 
-  if (!image) throw createHttpError(404, `Image ${image_id} not found`)
+  if (!image) throw createHttpError(404, `Image ${image_id} not found`);
   if (!user_is_admin && user_id.toString() !== image.uploader_id) {
-    throw createHttpError(403, `Unauthorized to update image`)
+    throw createHttpError(403, `Unauthorized to update image`);
   }
 
   const updatedImage = await Image.findOneAndUpdate(
     { _id: image_id },
     { $set: { ...new_properties } },
     { new: true }
-  )
+  );
 
-  removeImageRecordFromCache(updatedImage._id)
-  res.send(updatedImage)
-}
+  removeImageRecordFromCache(updatedImage._id);
+  res.send(updatedImage);
+};
 
 export const delete_image = async (req: Request, res: Response) => {
-  const { user } = res.locals
-  if (!user) throw createHttpError(403, `Unauthorized to delete image`)
+  const { user } = res.locals;
+  if (!user) throw createHttpError(403, `Unauthorized to delete image`);
 
-  const user_id = getUserId(res)
-  const user_is_admin = user.isAdmin || user.properties.isAdmin
+  const user_id = getUserId(res);
+  const user_is_admin = user.isAdmin || user.properties?.isAdmin;
 
-  const image_id = req.params.id
+  const image_id = req.params.id;
 
-  const image = await Image.findById(image_id)
-  if (!image) throw createHttpError(404, `Image ${image_id} not found`)
+  const image = await Image.findById(image_id);
+  if (!image) throw createHttpError(404, `Image ${image_id} not found`);
 
   if (!user_is_admin && user_id.toString() !== image.uploader_id) {
-    throw createHttpError(403, `Unauthorized to delete image`)
+    throw createHttpError(403, `Unauthorized to delete image`);
   }
 
   try {
-    if (s3Client) deleteFileFromS3(image)
-    else deleteLocalImage(image)
+    if (s3Client) deleteFileFromS3(image);
+    else deleteLocalImage(image);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 
-  await image.remove()
-  removeImageRecordFromCache(image._id)
+  await image.remove();
+  removeImageRecordFromCache(image._id);
 
-  res.send({ image_id })
-}
+  res.send({ image_id });
+};
 
 const save_views = async (req: Request, imageId: string) => {
   try {
-    const image: ImageRecord = await Image.findById(imageId)
+    const image: ImageRecord = await Image.findById(imageId);
 
     // Increase view count
-    if (!image.views) image.views = 0
-    image.views++
+    if (!image.views) image.views = 0;
+    image.views++;
 
     // Save last view data
-    image.last_viewed = new Date()
+    image.last_viewed = new Date();
 
     // save referer
-    const referer_url = req.get("Referrer")
+    const referer_url = req.get("Referrer");
 
     if (referer_url) {
       const found_referer = image.referers.find(
         ({ url }: { url: string }) => url === referer_url
-      )
+      );
 
       if (found_referer) {
-        found_referer.last_request = new Date()
-        if (!found_referer.views) found_referer.views = 0
-        found_referer.views++
+        found_referer.last_request = new Date();
+        if (!found_referer.views) found_referer.views = 0;
+        found_referer.views++;
       } else {
         image.referers.push({
           url: referer_url,
           last_request: new Date(),
           views: 1,
-        })
+        });
       }
     }
 
-    await image.save()
+    await image.save();
   } catch (error) {
-    console.log(`Failed to update view count for image ${imageId}`)
-    console.log(error)
+    console.log(`Failed to update view count for image ${imageId}`);
+    console.log(error);
   }
-}
+};
